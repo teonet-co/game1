02cfbfafd1a569125b8e4c0800c4fa2ba376c3c4
max <mpano91@gmail.com>
2018-08-08 01:25:20 +0300

    test area 12

--- tdrl/Assets/Maps/red_collision.tsx
+++ tdrl/Assets/Maps/red_collision.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="red_collision" tilewidth="16" tileheight="16" spacing="1" tilecount="1" columns="1">
  <image source="../Art/red-collision-box.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="2" x="-0.1875" y="-0.125" width="16.1875" height="16.1875"/>
+  </objectgroup>
+ </tile>
 </tileset>
