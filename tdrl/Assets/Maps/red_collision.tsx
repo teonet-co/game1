b0eac9f2a4bd6c8ce135449ff0b2d95665f3dbae
max <mpano91@gmail.com>
2018-08-08 01:04:33 +0300

    test area 7

---
+++ tdrl/Assets/Maps/red_collision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red_collision" tilewidth="16" tileheight="16" spacing="1" tilecount="1" columns="1">
+ <image source="../Art/red-collision-box.png" width="16" height="16"/>
+</tileset>
---
+++ tdrl/Assets/Maps/tower.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tower" tilewidth="16" tileheight="16" spacing="1" tilecount="56" columns="8">
+ <image source="../../../../../../../Downloads/Telegram Desktop/tower.png" width="139" height="122"/>
+</tileset>
