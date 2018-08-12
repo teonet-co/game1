a681c7307b60e87ff23c8d3319e75d47b0011684
max <mpano91@gmail.com>
2018-08-12 16:55:35 +0300

    Load new area.\n Simple start point.

--- tdrl/Assets/Maps/base tileset.tsx
+++ tdrl/Assets/Maps/base tileset.tsx
@@ -1,9 +1,15 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="base tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
- <image source="tdrl/Assets/Art/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <image source="../Art/roguelikeSheet_transparent.png" width="968" height="526"/>
  <terraintypes>
   <terrain name="Dirt" tile="578"/>
+  <terrain name="pool" tile="231"/>
  </terraintypes>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="3">
   <objectgroup draworder="index">
    <object id="10" x="0.0625" y="0.125" width="15.75" height="15.75"/>
@@ -45,16 +51,72 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="173" terrain=",,,1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="174" terrain=",,1,1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="175" terrain=",,1,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="230" terrain=",1,,1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="231" terrain="1,1,1,1">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="232" terrain="1,,1,">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="287" terrain=",1,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="288" terrain="1,1,,">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="289" terrain="1,,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
  <tile id="518" terrain="0,0,0,"/>
  <tile id="519" terrain="0,0,,0"/>
  <tile id="520" terrain=",,,0"/>
  <tile id="521" terrain=",,0,0"/>
  <tile id="522" terrain=",,0,"/>
+ <tile id="526">
+  <objectgroup draworder="index">
+   <object id="2" x="0.3125" y="0.9375" width="15.1875" height="15.1875"/>
+  </objectgroup>
+ </tile>
  <tile id="575" terrain="0,,0,0"/>
  <tile id="576" terrain=",0,0,0"/>
  <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
  <tile id="579" terrain="0,,0,"/>
  <tile id="634" terrain=",0,,"/>
  <tile id="635" terrain="0,0,,"/>
  <tile id="636" terrain="0,,,"/>
+ <tile id="643">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
