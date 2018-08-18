20bd1060a93f39b5f8790981f965d9945b4b1c25
max <mpano91@gmail.com>
2018-08-18 16:00:56 +0300

    New map

---
+++ tdrl/Assets/Maps/scetchup_background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="scetchup_background" tilewidth="16" tileheight="16" margin="1" tilecount="1749" columns="33">
+ <image source="C:/Users/KirMitrich/Desktop/undying escape/first_level copy.png" width="530" height="862"/>
+</tileset>
